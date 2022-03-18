dbafafe01c2351d307c2aa71b5240c14ea220932
YoshiBNE <yoshiyasu_express@me.com>
2022-03-19 00:22:43 +1000

    Done

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 // Your code here
-// const dodger = document.getElementById("dodger");
 
-// dodger.style.backgroundColor = "#000000";
-
-// document.addEventListener("keydown", function(event) {
-//     if (event.key === "ArrowLeft") {
-//         const leftNumbers = dodger.style.left.replace("px", "");
-//         const left = parseInt(leftNumbers, 10);
-
-//         dodger.style.left = `${left - 1}px`;
-//     }
-// })
 
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
